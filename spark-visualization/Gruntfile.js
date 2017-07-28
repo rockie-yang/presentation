709e965ce634c209f19d8b72fc9da5d61c5d0c2b
Rockie Yang <eyouyan@gmail.com>
2017-07-28 10:15:01 +0200

    update

--- spark-visualization/Gruntfile.js
+++ spark-visualization/Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 9000,
+                port: 3001,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
             },
